07512997edca45dd12da603c27e19d966e44120e
Marko Baskarad <markobaskarad95@hotmail.com>
2020-09-25 19:49:08 +0200

    move ajax api calls into a separate files for vehiclemake and vehiclemodel

--- src/stores/VehicleMakeStore.jsx
+++ src/stores/VehicleMakeStore.jsx
@@ -1,41 +1,43 @@
 import { observable, action } from "mobx";
-import axios from "axios";
+import { vehicleMakeService } from "../common/services/VehicleMakeService";
 
 class VehicleMakeStore {
   @observable loading = false;
   @observable loadingVehicles = false;
+
   @observable vehicleMakes = [];
+  @observable vehicle = { VehicleMakeId: null, Name: "", Abbreviation: "" };
+
   @observable sortBy = "";
   @observable searchString = "";
-  @observable isVehicleCreated = false;
-  @observable isVehicleUpdated = false;
-  @observable vehicleError = null;
-  @observable isDeleted = false;
   @observable pagingInfo = {
     resultsPerPage: 0,
     totalCount: 0,
     pageNumber: 1,
   };
 
-  @observable vehicle = { VehicleMakeId: null, Name: "", Abbreviation: "" };
+  @observable isVehicleCreated = false;
+  @observable isVehicleUpdated = false;
+  @observable isDeleted = false;
+  @observable vehicleError = null;
 
   @action async getVehicleMakes() {
     try {
       this.loadingVehicles = true;
-      const results = await axios.get("api/vehiclemake", {
-        params: {
-          sortBy: this.sortBy,
-          searchString: this.searchString,
-          page: this.pagingInfo.pageNumber,
-        },
-      });
+      const results = await vehicleMakeService.getVehicleMakes(
+        this.sortBy,
+        this.searchString,
+        this.pagingInfo.pageNumber
+      );
 
       this.vehicleMakes = results.data.vehicles;
+
       this.pagingInfo = {
         resultsPerPage: results.data.pagingInfo.resultsPerPage,
         totalCount: results.data.pagingInfo.totalCount,
         pageNumber: results.data.pagingInfo.pageNumber,
       };
+
       this.loadingVehicles = false;
     } catch (error) {
       this.loadingVehicles = false;
@@ -46,10 +48,9 @@ class VehicleMakeStore {
   @action async getVehicleMakeById(id) {
     try {
       this.loadingVehicles = true;
-      const results = await axios.get(`/api/vehiclemake/${id}`);
+      const results = await vehicleMakeService.getVehicleMakeById(id);
 
       this.vehicle = { ...results.data };
-
       this.loadingVehicles = false;
     } catch (error) {
       this.loadingVehicles = false;
@@ -60,10 +61,8 @@ class VehicleMakeStore {
   @action async createVehicleMake(vehicleMake) {
     try {
       this.loading = true;
-      await axios.post("/api/vehiclemake", {
-        name: vehicleMake.name,
-        abbreviation: vehicleMake.abrv,
-      });
+      const make = { name: vehicleMake.name, abrv: vehicleMake.abrv };
+      await vehicleMakeService.createVehicleMake(make);
 
       this.isVehicleCreated = true;
       this.loading = false;
@@ -76,13 +75,12 @@ class VehicleMakeStore {
   @action async editVehicleMake(vehicleMake, id) {
     try {
       this.loading = true;
-      await axios.put(`/api/vehiclemake/${id}`, {
-        id: id,
-
+      const vehicle = {
         VehicleMakeId: this.vehicle.VehicleMakeId,
         Name: vehicleMake.name,
         Abbreviation: vehicleMake.abrv,
-      });
+      };
+      await vehicleMakeService.editVehicleMake(id, vehicle);
 
       this.isVehicleUpdated = true;
       this.loading = false;
@@ -95,9 +93,7 @@ class VehicleMakeStore {
   @action async deleteVehicleMake(id) {
     try {
       this.loading = true;
-      await axios.delete(`/api/vehiclemake/${id}`, {
-        id: id,
-      });
+      await vehicleMakeService.deleteVehicleMake(id);
 
       this.isDeleted = true;
       this.loading = false;
--- src/stores/VehicleModelStore.jsx
+++ src/stores/VehicleModelStore.jsx
@@ -1,55 +1,63 @@
 import { observable, action } from "mobx";
-import axios from "axios";
+import { vehicleModelService } from "../common/services/VehicleModelService";
 
 class VehicleModelStore {
   @observable vehicleModels = [];
-  @observable sortBy = "";
-  @observable searchString = "";
-  @observable page = 1;
-  @observable loading = false;
-  @observable loadingVehicles = false;
-  @observable isVehicleCreated = false;
-  @observable isVehicleUpdated = false;
-  @observable vehicleError = null;
-  @observable isDeleted = false;
-  @observable pagingInfo = {
-    resultsPerPage: 0,
-    totalCount: 0,
-    pageNumber: 1,
-  };
+
   @observable vehicle = {
     VehicleMakeId: "",
     Name: "",
     Abbreviation: "",
   };
 
+  @observable sortBy = "";
+  @observable searchString = "";
+  @observable page = 1;
+
+  @observable pagingInfo = {
+    resultsPerPage: 0,
+    totalCount: 0,
+    pageNumber: 1,
+  };
+
+  @observable loading = false;
+  @observable loadingVehicles = false;
+
+  @observable isVehicleCreated = false;
+  @observable isVehicleUpdated = false;
+  @observable vehicleError = null;
+  @observable isDeleted = false;
+
   @action async getVehicleModels() {
     try {
       this.loadingVehicles = true;
-      const results = await axios.get("api/vehiclemodel", {
-        params: {
-          sortBy: this.sortBy,
-          searchString: this.searchString,
-          page: this.pagingInfo.pageNumber,
-        },
-      });
+
+      const results = await vehicleModelService.getVehicleModels(
+        this.sortBy,
+        this.searchString,
+        this.pagingInfo.pageNumber
+      );
 
       this.vehicleModels = results.data.vehicles;
+
       this.pagingInfo = {
         resultsPerPage: results.data.pagingInfo.resultsPerPage,
         totalCount: results.data.pagingInfo.totalCount,
         pageNumber: results.data.pagingInfo.pageNumber,
       };
+
       this.loadingVehicles = false;
     } catch (error) {
       this.loadingVehicles = false;
       this.vehicleError = "Unable to fetch the vehicles";
     }
   }
+
   @action async getVehicleModelById(id) {
     try {
       this.loadingVehicles = true;
-      const results = await axios.get(`/api/vehiclemodel/${id}`);
+
+      const results = await vehicleModelService.getVehicleModelById(id);
 
       this.vehicle = { ...results.data };
 
@@ -59,15 +67,17 @@ class VehicleModelStore {
       this.vehicleError = "Unable to fetch the vehicle";
     }
   }
+
   @action async createVehicleModel(vehicleModel) {
     try {
       this.loading = true;
 
-      await axios.post("/api/vehiclemodel", {
-        vehiclemakeid: vehicleModel.vehiclemakeid,
+      const model = {
         name: vehicleModel.name,
-        abbreviation: vehicleModel.abrv,
-      });
+        abrv: vehicleModel.abrv,
+        vehiclemakeid: vehicleModel.vehiclemakeid,
+      };
+      await vehicleModelService.createVehicleModel(model);
 
       this.isVehicleCreated = true;
       this.loading = false;
@@ -76,16 +86,17 @@ class VehicleModelStore {
       this.vehicleError = error;
     }
   }
+
   @action async editVehicleModel(vehicleModel, id) {
     try {
       this.loading = true;
-      await axios.put(`/api/vehiclemodel/${id}`, {
-        id: id,
 
-        vehiclemakeid: vehicleModel.vehiclemakeid,
+      const vehicle = {
+        VehicleMakeId: this.vehicle.VehicleMakeId,
         Name: vehicleModel.name,
         Abbreviation: vehicleModel.abrv,
-      });
+      };
+      await vehicleModelService.editVehicleModel(id, vehicle);
 
       this.isVehicleUpdated = true;
       this.loading = false;
@@ -94,12 +105,11 @@ class VehicleModelStore {
       this.vehicleError = error;
     }
   }
+
   @action async deleteVehicleModel(id) {
     try {
       this.loading = true;
-      await axios.delete(`/api/vehiclemodel/${id}`, {
-        id: id,
-      });
+      await vehicleModelService.deleteVehicleModel(id);
 
       this.isDeleted = true;
       this.loading = false;
